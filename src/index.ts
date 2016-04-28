8a14f5f013277d55ff07d726cfe1d80fb95fc72a
jelisejev <pavel.yeliseev@ctco.lv>
2016-04-28 22:21:26 +0300

    implemented color section

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,34 @@ class CssStatsComponent {
   }
   
   private renderResults(data: CssStatsData) {
-    const {rules, selectors} = data.stats;
+    const {rules, selectors, declarations} = data.stats;
     
     this.elem.find('.results').removeClass('hidden');
     this.elem.find('.rules').text(rules.total);
     this.elem.find('.selectors').text(selectors.total);
+    
+    this.renderColors(declarations);
   }  
+  
+  private renderColors(declarations: Declarations) {
+    const colorContainer: JQuery = this.elem.find('.colors');
+    this.getUniqueColors(declarations).forEach((color: string) => {
+      $('<div class="col-md-3"></div>')
+        .text(color)
+        .css('background', color)
+        .appendTo(colorContainer);
+    });
+  }
+  
+  private getUniqueColors(declarations: Declarations) {
+    return declarations.properties.color.reduce((uniqueColors: Array<string>, color: string) => {
+      if (uniqueColors.indexOf(color) === -1) {
+        uniqueColors.push(color);
+      }
+      
+      return uniqueColors;
+    }, []);
+  }
 }
 
 interface CssStatsData {
@@ -29,8 +51,15 @@ interface CssStatsData {
     };
     selectors: {
       total: number;
-    }
+    },
+    declarations: Declarations;
   }
+}
+
+interface Declarations {
+  properties: {
+    color: Array<string>  
+  };
 }
 
 new CssStatsComponent($('#css-stats')).init();
