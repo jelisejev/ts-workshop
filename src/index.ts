ac5454f06be65795a9cdcf25c62891a0aac6e4f1
jelisejev <pavel.yeliseev@ctco.lv>
2016-04-28 22:16:11 +0300

    added jquery, implemented data loading

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,36 @@
 
 class CssStatsComponent {
-  public constructor(private elem: HTMLElement) {
+  protected api: string = 'http://localhost:8080';
+  
+  public constructor(private elem: JQuery) {
   }
 
   init() {
-    console.log(this.elem);
+    this.elem.find('.analyze').click(() => this.loadStats($('input', this.elem).val()));
+  }
+
+  protected loadStats(url: string) {
+    return $.get(this.api + '/stats', { url }).then((data) => this.renderResults(data));
   }
   
+  private renderResults(data: CssStatsData) {
+    const {rules, selectors} = data.stats;
+    
+    this.elem.find('.results').removeClass('hidden');
+    this.elem.find('.rules').text(rules.total);
+    this.elem.find('.selectors').text(selectors.total);
+  }  
 }
 
-new CssStatsComponent(document.getElementById('css-stats')).init();
+interface CssStatsData {
+  stats: {
+    rules: {
+      total: number;
+    };
+    selectors: {
+      total: number;
+    }
+  }
+}
+
+new CssStatsComponent($('#css-stats')).init();
